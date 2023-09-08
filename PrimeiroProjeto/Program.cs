3765d8c5abb1f0847e04bb609dae893871165dee
Brendo Lopez <brendoslopes@gmail.com>
2023-09-08 08:19:07 -0300

    Mais exercicios.

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,10 +7,24 @@ namespace curso
     {
         static void Main(string[] args)
         {
-            int X = int.Parse(Console.ReadLine());
-            int Y = int.Parse(Console.ReadLine());
-            while (true) {
-                
+            double idadeUsuario, soma, media;
+            int contagem;
+            idadeUsuario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            soma = 0.0;
+            contagem = 0;
+            while (idadeUsuario >= 0)
+            {
+                soma = soma + idadeUsuario;
+                contagem++;
+                idadeUsuario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            if (contagem == 0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            } else
+            {
+                media = soma / contagem;
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
             }
         }
     }
