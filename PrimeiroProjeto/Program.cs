37295e1ac027917a0851106bdd263a8ea6abd260
Brendo S. Lopes <brendoslopes@gmail.com>
2023-09-12 14:04:11 -0300

    Testes

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,25 +7,7 @@ namespace curso
     {
         static void Main(string[] args)
         {
-            double idadeUsuario, soma, media;
-            int contagem;
-            idadeUsuario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            soma = 0.0;
-            contagem = 0;
-            while (idadeUsuario >= 0)
-            {
-                soma = soma + idadeUsuario;
-                contagem++;
-                idadeUsuario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            }
-            if (contagem == 0)
-            {
-                Console.WriteLine("Impossivel calcular");
-            } else
-            {
-                media = soma / contagem;
-                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
-            }
+
         }
     }
 }
