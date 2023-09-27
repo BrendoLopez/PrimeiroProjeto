08b3683070e528b6b92153a41b84f3c35a1adfac
Brendo Lopez <brendoslopes@gmail.com>
2023-09-27 18:15:22 -0300

    Vetor e For.

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,37 +7,28 @@ namespace curso
     {
         static void Main(string[] args)
         {
-            double notaAluno1 = -1;
-            double notaAluno2 = -1;
+            int N = int.Parse(Console.ReadLine());
+            int X;
+            int[] vet;
+            vet = new int[N];
+            string[] S = Console.ReadLine().Split(' ');
 
-            while (true)
+            for (int i = 0; i < N; i++)
             {
-                notaAluno1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                if (notaAluno1 >= 0 && notaAluno1 <= 10)
+                vet[i] = int.Parse(S[i]);
+            }
+            //for (int i = 0; i < 1; i++)
+            //{
+            //    X = int.Parse(Console.ReadLine());
+            //    vet[i] = X;
+            //}
+            for (int i = 0; i < N; i++)
+            {
+                if (vet[i] < 0)
                 {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("nota invalida");
+                    Console.WriteLine(vet[i]);
                 }
             }
-            while (true)
-            {
-                notaAluno2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                if (notaAluno2 >= 0 && notaAluno2 <= 10)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("nota invalida");
-                }
-            }
-            double somaMedia = (notaAluno1 + notaAluno2) / 2;
-            Console.WriteLine("media = " + somaMedia.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
