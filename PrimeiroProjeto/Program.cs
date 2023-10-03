330484bb5dd9d1418d0d8a31d892bbd259b8b443
Brendo Lopez <brendoslopes@gmail.com>
2023-10-03 18:43:38 -0300

    Commit de alterações e mais exercicios.

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,28 +7,7 @@ namespace curso
     {
         static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
-            int X;
-            int[] vet;
-            vet = new int[N];
-            string[] S = Console.ReadLine().Split(' ');
 
-            for (int i = 0; i < N; i++)
-            {
-                vet[i] = int.Parse(S[i]);
-            }
-            //for (int i = 0; i < 1; i++)
-            //{
-            //    X = int.Parse(Console.ReadLine());
-            //    vet[i] = X;
-            //}
-            for (int i = 0; i < N; i++)
-            {
-                if (vet[i] < 0)
-                {
-                    Console.WriteLine(vet[i]);
-                }
-            }
         }
     }
 }
