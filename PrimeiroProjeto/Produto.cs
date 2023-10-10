f7c4a58f033b846984071bb851736778dcb47106
Brendo Lopez <brendoslopes@gmail.com>
2023-10-10 19:45:33 -0300

    Novos exercicios de Classe, métodos, atributos e membros estáticos.

--- PrimeiroProjeto/Produto.cs
+++ PrimeiroProjeto/Produto.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PrimeiroProjeto;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +27,43 @@ namespace PrimeiroProjeto
         public void RemoverProdutos(int quantidade)
         {
             Quantidade = Quantidade - quantidade;
+        }
+    }
+}
+
+namespace curso
+{
+    class Exercicio1
+    {
+        static void Exercicio(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do produto: {produto.Nome}, $ {produto.Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
+            $"{produto.Quantidade} unidades," + " Total: $ " + produto.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Dados do produto: {produto.Nome}, $ {produto.Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
+            $"{produto.Quantidade} unidades," + " Total: $ " + produto.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.Write("Digite o número de produtos a ser removidos do estoque: ");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Dados do produto: {produto.Nome}, $ {produto.Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
+            $"{produto.Quantidade} unidades," + " Total: $ " + produto.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -8,33 +8,7 @@ namespace curso
     {
         static void Main(string[] args)
         {
-            Produto produto = new Produto();
 
-            Console.WriteLine("Entre os dados do produto:");
-            Console.Write("Nome: ");
-            produto.Nome = Console.ReadLine();
-
-            Console.Write("Preço: ");
-            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.Write("Quantidade no estoque: ");
-            produto.Quantidade = int.Parse(Console.ReadLine());
-
-            Console.WriteLine($"Dados do produto: {produto.Nome}, $ {produto.Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
-            $"{produto.Quantidade} unidades," + " Total: $ " + produto.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture));
-
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-
-            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
-
-            Console.WriteLine($"Dados do produto: {produto.Nome}, $ {produto.Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
-            $"{produto.Quantidade} unidades," + " Total: $ " + produto.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture));
-
-            Console.Write("Digite o número de produtos a ser removidos do estoque: ");
-            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
-
-            Console.WriteLine($"Dados do produto: {produto.Nome}, $ {produto.Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
-            $"{produto.Quantidade} unidades," + " Total: $ " + produto.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
