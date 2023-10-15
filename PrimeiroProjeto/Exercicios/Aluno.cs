27f44d103736cb5aeb5760011609a67c6cfb64ef
Brendo Lopez <brendoslopes@gmail.com>
2023-10-15 19:21:28 -0300

    Exercicios.

---
+++ PrimeiroProjeto/Exercicios/Aluno.cs
@@ -0,0 +1,80 @@
+﻿using PrimeiroProjeto;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    public class Aluno
+    {
+        public string Nome;
+        public double NotaPrimeiroTrimestre;
+        public double NotaSegundoTrimestre;
+        public double NotaTerceiroTrimestre;
+
+
+        public double NotaFinal()
+        {
+            return NotaPrimeiroTrimestre + NotaSegundoTrimestre + NotaTerceiroTrimestre;
+        }
+
+        public bool Aprovacao()
+        {
+            if (NotaFinal() >= 60)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double NotaRestante()
+        {
+            if (Aprovacao())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - NotaFinal();
+            }
+        }
+    }
+}
+
+namespace curso
+{
+    class ExercicioAluno
+    {
+        static void Aluno(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do Aluno:");
+
+            Console.Write("Nota primeiro trimestre: ");
+            aluno.NotaPrimeiroTrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Nota segundo trimestre: ");
+            aluno.NotaSegundoTrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Nota terceiro trimestre: ");
+            aluno.NotaTerceiroTrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (aluno.Aprovacao())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS.");
+            }
+        }
+    }
+}
---
+++ PrimeiroProjeto/Exercicios/ConversorDeMoeda.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    public static class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double DolarParaReal(double cotacao, double quantia)
+        {
+            double total = cotacao * quantia;
+            return total + total * Iof / 100.0;
+        }
+    }
+
+    namespace curso
+    {
+        class Estaticos
+        {
+            static void ExercicioMembrosEstaticos(string[] args)
+            {
+                Console.Write("Qual é a cotação do dólar? ");
+                double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Quantos dólares você vai comprar? ");
+                double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                double resultado = ConversorDeMoeda.DolarParaReal(cotacao, quantia);
+                Console.WriteLine("Valor a ser pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
