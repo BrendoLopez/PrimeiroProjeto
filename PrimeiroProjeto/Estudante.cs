ff3b89ce02024f26d8deca9e16c3ca6fa87b61be
Brendo Lopez <brendoslopes@gmail.com>
2023-10-24 21:38:30 -0300

    Exercicios

---
+++ PrimeiroProjeto/Estudante.cs
@@ -0,0 +1,63 @@
+﻿using PrimeiroProjeto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto
+{
+    internal class Estudante
+    {
+        public string Nome{ get; set; }
+        public string Email{ get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: "+ Nome + ", " + "Email: "+ Email;
+        }
+    }
+}
+
+namespace ExercicioEstudante
+{
+    class EstudanteExercicio
+    {
+        static void ExercicioEstudant(string[] args)
+        {
+            Estudante[] vetor = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+
+            int numeroDeEstudantes = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numeroDeEstudantes; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vetor[quarto] = new Estudante(nome, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vetor[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vetor[i]);
+                }
+            }
+        }
+    }
+}
---
+++ PrimeiroProjeto/Exercicios/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioProduct
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    class ExercicioVetor
+    {
+        static void VetorComObjeto(string[] args)
+        {
+            int quantidadeDeProdutos = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[quantidadeDeProdutos];
+
+            for (int i = 0; i < quantidadeDeProdutos; i++)
+            {
+                string nameProduto = Console.ReadLine();
+                double valorProduto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = nameProduto, Price = valorProduto };
+            }
+
+            double somaValorProdutos = 0.0;
+
+            for (int i = 0; i < quantidadeDeProdutos; i++)
+            {
+                somaValorProdutos += vect[i].Price;
+            }
+            Console.WriteLine(somaValorProdutos / quantidadeDeProdutos);
+        }
+    }
+}
